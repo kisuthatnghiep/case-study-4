1ba60ed5385d7daa15562a32cc552023b0811b98
unknown <ntthanhdb1999@gmail.com>
2023-01-05 16:59:29 +0700

    update change password

--- src/main/java/com/example/module_4/controller/LoginController.java
+++ src/main/java/com/example/module_4/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.module_4.controller;
 
 import com.example.module_4.model.User;
+import com.example.module_4.model.UserUpdate;
 import com.example.module_4.service.LoginService;
 import com.example.module_4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,27 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @PostMapping ("/login")
+    @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody User user) {
         if (loginService.login(user)) {
             return new ResponseEntity<>(userService.findUserByUsername(user.getUsername()), HttpStatus.OK);
         }
-        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
     @PostMapping("/signUp")
-    public ResponseEntity<String> signUp(@RequestBody User user){
-        if (loginService.signUp(user)){
-        return new ResponseEntity<>("Sign up successfully!", HttpStatus.CREATED);
+    public ResponseEntity<String> signUp(@RequestBody User user) {
+        if (loginService.signUp(user)) {
+            return new ResponseEntity<>("Sign up successfully!", HttpStatus.CREATED);
         }
-        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/changePw")
+    public ResponseEntity<String> changePassword(@RequestBody UserUpdate userUpdate) {
+        if (loginService.changePassword(userUpdate)) {
+            return new ResponseEntity<>("Change password successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
